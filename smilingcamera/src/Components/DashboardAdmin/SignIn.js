6925e37e83437ad5a13f024f9b19ad6e0fe3f657
Angel Torres <angeltorres7850@gmail.com>
2019-09-19 22:10:49 -0500

    ambas partes

--- smilingcamera/src/Components/DashboardAdmin/SignIn.js
+++ smilingcamera/src/Components/DashboardAdmin/SignIn.js
@@ -1,3 +1,168 @@
+/*import React from 'react';
+import { render } from "react-dom";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+class SignIn extends React.Component {
+  
+  
+  constructor() {
+    super();
+    this.state = {};
+    
+  }
+
+  render() {
+    
+    return (
+      
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div>
+        <Avatar >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form onSubmit={this.onSubmit} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="userName"
+            autoComplete="email"
+            autoFocus
+            onChange={this.onUserNameChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="passwordUser"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={this.onUserPassChange}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+    );
+  }
+
+  onUserNameChange = e => {
+    this.setState({ userName: e.target.value });
+  };
+  onUserPassChange = e => {
+    this.setState({ passwordUser: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    let data = {
+      mainEmail: this.state.userName,
+      passwordUser: this.state.passwordUser
+    }
+    fetch("http://" + document.domain+":3500/logIn",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ mainEmail: this.state.userName, passwordUser: this.state.passwordUser })
+    }).then(function (response) {
+        return response.json(); // call the json method on the response to get JSON
+    })
+    .then(function (json) {
+        if(json.Status =="S")
+        console.log(json.Status);
+    })
+  };
+}
+export default SignIn;*/
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -82,60 +247,10 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(3, 0, 2),
   },
 }));
-onUserNameChange = e => {
-  this.setState({ userName: e.target.value });
-};
-onUserPassChange = e => {
-  this.setState({ passwordUser: e.target.value });
-};
-onSubmit = e => {
-  e.preventDefault();
-  let data = {
-    mainEmail: this.state.userName,
-    passwordUser: this.state.passwordUser
-  }
-  fetch("http://" + document.domain+":3500/logIn",{
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ mainEmail: this.state.userName, passwordUser: this.state.passwordUser })
-  }).then(function (response) {
-      return response.json(); // call the json method on the response to get JSON
-  })
-  .then(function (json) {
-      
-      console.log(json.Status);
-  })
-};
-function onUserNameChange(e){
-  this.setState({ userName: e.target.value });
-}
-function onUserPassChange(e){
-  this.setState({ passwordUser: e.target.value });
-}
-function onSubmit(e){
-  e.preventDefault();
-  let data = {
-    mainEmail: this.state.userName,
-    passwordUser: this.state.passwordUser
-  }
-  fetch("http://" + document.domain+":3500/logIn",{
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ mainEmail: this.state.userName, passwordUser: this.state.passwordUser })
-  }).then(function (response) {
-      return response.json(); // call the json method on the response to get JSON
-  })
-  .then(function (json) {
-      
-      console.log(json.Status);
-  })
-}
+
 export default function SignIn() {
   const classes = useStyles();
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -162,19 +277,17 @@ export default function SignIn() {
             name="email"
             autoComplete="email"
             autoFocus
-            onChange={this.onUserNameChange}
           />
           <TextField
             variant="outlined"
             margin="normal"
             required
             fullWidth
-            name="passwordUser"
+            name="password"
             label="Contraseña"
             type="password"
             id="password"
             autoComplete="current-password"
-            onChange={this.onUserPassChange}
           />
           {/*<FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
@@ -199,43 +312,5 @@ export default function SignIn() {
         <Copyright />
       </Box>
     </Container>
-    );
-  }
-
-  
-  
-  
-
-  /*
-
-//export default SignIn;
-/*import React from "react";
-import { render } from "react-dom";
-const useStyles = makeStyles(theme => ({
-      '@global': {
-        body: {
-          backgroundColor: theme.palette.common.white,
-        },
-      },
-      paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      },
-      avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-      },
-      form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-      },
-      submit: {
-        margin: theme.spacing(3, 0, 2),
-      },
-    }));
-    const classes = useStyles();
-const classes = useStyles();
-
- */
+  );
+}
