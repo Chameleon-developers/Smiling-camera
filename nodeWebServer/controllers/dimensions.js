5b4d14bdcf348b4657997face6a5a9d23a6cc79e
Joel Sabethai <joel.sabethai@gmail.com>
2019-10-04 01:15:49 -0500

    ws getDimension and load of selects

---
+++ nodeWebServer/controllers/dimensions.js
@@ -0,0 +1,34 @@
+/* Obtener las dimensiones existentes */
+module.exports.getDimensions = function (req, res) {
+    /* Obtener variable para la conexión a la BD */
+    const con = require('../controllers/dbconn')();
+
+    /* Ejecutar la consulta para la obtención de dimensiones */
+    con.query('SELECT idDimension, CONCAT(widthDimension, "X", heightDimension) AS dimension FROM dimensions', function (err, result, fields) {
+        if (err) {
+            // Internal error message send
+            res.status(500).json({
+                Status: 'Internal Error',
+                message: 'Internal Error'
+            });
+            con.end();
+            return;
+        } else {
+            if (result.length > 0) {
+
+                // Setup and send of response
+                res.status(200).json({
+                    Status: 'Success',
+                    dimensions: result,
+                    message: 'Datos de las dimensiones'
+                })
+            } else {
+                res.status(400).json({
+                    Status: 'Failure',
+                    message: 'No existen dimensiones para e-commerce YouPrint'
+                })
+                con.end();
+            }
+        }
+    });
+}
