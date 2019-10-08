83d356efa848baf695019c81b07c1036f988e91a
JoelSabethai <42782741+JoelSabethai@users.noreply.github.com>
2019-10-07 20:42:18 -0500

    ws deleteKiosco and updateKiosco

--- nodeWebServer/controllers/kioscos.js
+++ nodeWebServer/controllers/kioscos.js
@@ -112,6 +112,78 @@ module.exports.insertKiosco = function (req, res) {
     }
 }
 
+/* Actualizar datos de un kiosco */
+module.exports.updateKiosco = function(req, res) {
+     /* Obtener variable para la conexión a la BD */
+    const con = require('../controllers/dbconn').db_prom();
+
+    /* Obtener los datos del Body */
+    let data = req.body;
+    
+    /* Ejecutar la consulta para eliminar kiosco */
+    con.query('UPDATE kioscos SET nameKiosco=? WHERE idKiosco=?', [data.nameKiosco, data.idKiosco] function (err, result, fields) {
+        if (err) {
+            // Internal error message send
+            res.status(500).json({
+                Status: 'Internal Error',
+                message: 'Internal Error'
+            });
+            con.end();
+            return;
+        } else {
+            if (result.length == 1) {
+                // Setup and send of response
+                res.status(200).json({
+                    Status: 'Success',
+                    message: 'Se actualizo correctamente el kiosco'
+                })
+            } else {
+                res.status(400).json({
+                    Status: 'Failure',
+                    message: 'No existen kioscos registrados'
+                })
+                con.end();
+            }
+        }
+    });
+}
+
+/* Elimina kiosco (baja logica) */
+module.exports.deleteKiosco = function (req, res) {
+    /* Obtener variable para la conexión a la BD */
+    const con = require('../controllers/dbconn')();
+
+    /* Obtener los datos del Body */
+    let data = req.body;
+
+    /* Ejecutar la consulta para eliminar kiosco */
+    con.query('UPDATE kioscos SET statusKiosco=0 WHERE idKiosco=?', [data.idKiosco] function (err, result, fields) {
+        if (err) {
+            // Internal error message send
+            res.status(500).json({
+                Status: 'Internal Error',
+                message: 'Internal Error'
+            });
+            con.end();
+            return;
+        } else {
+            if (result.length == 1) {
+                // Setup and send of response
+                res.status(200).json({
+                    Status: 'Success',
+                    message: 'Se elimino correctamente el kiosco'
+                })
+            } else {
+                res.status(400).json({
+                    Status: 'Failure',
+                    message: 'No existen kioscos registrados'
+                })
+                con.end();
+            }
+        }
+    });
+}
+
 function validationsKioscos(nameUser, passwordUser) {
     if (nameUser == '' || passwordUser == '') {
         return false
